23558bab595b32fc359ace1b8090f9da8815db9c
derrick eckardt <derrick.eckardt@gmail.com>
2018-10-09 20:28:14 +0000

    started part1 in earnest

--- part1/betsy.py
+++ part1/betsy.py
@@ -1,4 +1,4 @@
-#!./usr/bin/env/ python
+#!/usr/bin/env python
 #
 # CS B551 - Elements of AI
 # Indiana University, Fall 2018
@@ -6,3 +6,28 @@
 #
 # Completed by Derrick Eckardt
 # derrick@iu.edu
+
+# Import libraries
+import sys
+
+# Get command line import
+n, current, board, time = [int(sys.argv[1]), str(sys.argv[2]), str(sys.argv[3]),int(sys.argv[4])]
+
+# Define possible next moves, successor function
+def moves(board):
+    # Drop a pebble
+        # Check if column is full
+    
+    # Rotate a column
+    return 0
+    
+def winner(board):
+    # Check row
+    
+    # Check column
+    
+    # Check diagonals
+    return 0
+    
+for c in "Sample Text":
+    print c
