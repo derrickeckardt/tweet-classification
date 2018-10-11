f01b6226d63c954c1bba1e3447e11b219763630e
derrick eckardt <derrick.eckardt@gmail.com>
2018-10-11 20:00:14 +0000

    Add check for winner in column

--- part1/betsy.py
+++ part1/betsy.py
@@ -13,21 +13,38 @@ import sys
 # Get command line input
 n, current, board, time = [int(sys.argv[1]), str(sys.argv[2]), str(sys.argv[3]),int(sys.argv[4])]
 
+# Implement heuristic function to determine strength of current player's move
+def score(board):
+    
+    return 1
+
+# See if board has a winning move
+def winner(board):
+    # Check row
+    
+    
+    # Check column, if True, a column has a winning move for the current player
+    columnn = max([True if current*n in ("".join([b for b in board[i:n*n:n]])) else False for i in range(n)])
+    print columnn
+
+    
+    
+    # Check diagonals
+    
+    return False
+    
+
 # Define possible next moves, successor function
 def moves(board):
+    new_boards = []
     # Drop a pebble
         # Check if column is full
     
     # Rotate a column
-    return 0
     
-def winner(board):
-    # Check row
+    new_boards.append([])        
     
-    # Check column
+    return new_boards
     
-    # Check diagonals
-    return 0
     
-for c in "Sample Text":
-    print c
+winner(board)
