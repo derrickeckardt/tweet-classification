b1155474c14ec377aef61a4200c558aaece125bc
derrick eckardt <derrick.eckardt@gmail.com>
2018-10-12 00:57:43 +0000

    sig progress on functions. working on successor

--- part1/betsy.py
+++ part1/betsy.py
@@ -13,6 +13,19 @@ import sys
 # Get command line input
 n, current, board, time = [int(sys.argv[1]), str(sys.argv[2]), str(sys.argv[3]),int(sys.argv[4])]
 
+# Utility function to get columns
+# getColumns(board,n,n) # gets winable part of columns
+# getColumns(board,n+3,n) # gets entire column
+def getColumns(board,m,n):
+    return [("".join([b for b in board[i:n*m:n]])) for i in range(n)]
+
+# Utility function to get rows
+# getRows(board,n) # Gets rows from winnable rows only
+# getRows(board,n+3) # gets rows from all rows
+def getRows(board,m,n):
+    return [("".join([b for b in board[i:i+n]])) for i in range(0,n*m,n)]
+
+
 # Implement heuristic function to determine strength of current player's move
 def score(board):
     
@@ -21,22 +34,15 @@ def score(board):
 # See if board has a winning move
 def winner(board):
     # Check row
-    
-    
-    # Check column, if True, a column has a winning move for the current player
-    columnn = max([True if current*n in ("".join([b for b in board[i:n*n:n]])) else False for i in range(n)])
-    print columnn
-    
-    columnn = [True if current*n in ("".join([b for b in board[i:n*n:n]])) else False for i in range(n)]
-    print columnn
+    row_win = True if current*n in getRows(board,n,n) else False
 
-    columnn = [("".join([b for b in board[i:n*n:n]])) for i in range(n)]
-    print columnn
-    
-    
+    # Check column, if True, a column has a winning move for the current player
+    column_win = True if current*n in getColumns(board,n,n) else False
+
     # Check diagonals
     
-    return False
+    # Just for readability, put this here,  Otherwise, would have combined all three of the above into one return line.
+    return max([row_win,column_win])
     
 
 # Define possible next moves, successor function
@@ -46,10 +52,14 @@ def moves(board):
         # Check if column is full
     
     # Rotate a column
+    column_boards = getColumns(board,n+3,n)
     
     new_boards.append([])        
     
     return new_boards
     
     
-winner(board)
+print winner(board)
+print moves(board)
+print "12345672".rindex("4")
+print "12345672".rindex("2")
